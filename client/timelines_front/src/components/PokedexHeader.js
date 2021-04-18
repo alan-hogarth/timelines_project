6b5d1afc451e7db4687c95dc66e81c3c0636af0e
Alan Hogarth <alan.hogarth@outlook.com>
2021-04-18 14:59:51 +0100

    chess icon to header

--- client/timelines_front/src/components/PokedexHeader.js
+++ client/timelines_front/src/components/PokedexHeader.js
@@ -5,7 +5,11 @@ const PokedexHeader = ({name}) => {
   
     return (
         <Segment basic inverted padded='very'>
-          <h1 className="ui massive center aligned header">Historical Timelines </h1>
+          <h1 className="ui massive center aligned header">
+            <span>
+              <i class="chess king icon"></i><br></br>  Historical Timelines  
+            </span>
+          </h1>
           <h3 className="ui center aligned header">  {name} </h3>
         </Segment>
         
